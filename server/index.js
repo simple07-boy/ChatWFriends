bea26ecc69711cdd251ec14d5b5cb1d4a408ea25
simple07-boy <142507697+simple07-boy@users.noreply.github.com>
2024-09-01 01:42:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,13 +7,7 @@ const socket = require("socket.io");
 const app = express();
 require("dotenv").config();
 
-app.use(cors(
-    {
-        origin: "http://chat-w-friends.vercel.app",
-        methods:["POST","GET"],
-        credentials: true,
-    }
-));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
